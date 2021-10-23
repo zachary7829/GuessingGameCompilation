042669d3d16676c8bd6bc6f70912fbeb4c8332f3
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2021-10-23 12:57:45 -0500

    Fixed main.m
    Oops! Accidentally uploaded c code for main.m. This should be Objective-C now.

--- main.m
+++ main.m
@@ -1,17 +1,21 @@
-#include <stdio.h>
+#import <Foundation/Foundation.h>
 
-int main(void) {
-  srand(time(NULL));
-  int minRange = 1;
-  int maxRange = 10;
-  int randvalue = rand()%((maxRange+1)-minRange) + minRange;
-  int guess;
-  printf("Enter a number between %d and %d: ",minRange,maxRange);
-  scanf("%d", &guess);
-  if (randvalue == guess){
-    printf("\nYou guessed %d. The correct number was %d. You win!\n",guess,randvalue);
-  } else {
-    printf("\nYou guessed %d. The correct number was %d. You loose!\n",guess,randvalue);
-  }
-  return 0;
+int main (int argc, const char * argv[])
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    int minRange = 1;
+    int maxRange = 10;
+    int randvalue = rand()%((maxRange+1)-minRange) + minRange;
+    int guess;
+    NSLog(@"Enter a number between %d and %d: ",minRange,maxRange);
+    scanf("%d", &guess);
+    if (randvalue == guess){
+      NSLog(@"\nYou guessed %d. The correct number was %d. You win!\n",guess,randvalue);
+    } else {
+      NSLog(@"\nYou guessed %d. The correct number was %d. You loose!\n",guess,randvalue);
+    }
+
+    [pool drain];
+    return 0;
 }
